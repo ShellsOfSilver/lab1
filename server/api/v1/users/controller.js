dffdb67ffbd57efee7a19b0d945de3cc66a74bf2
google <google@cloud.com>
2019-09-26 21:26:33 +0000

    delete napajs

--- server/api/v1/users/controller.js
+++ server/api/v1/users/controller.js
@@ -1,10 +1,5 @@
 const User = require('./model');
-const napa = require("napajs");
-
 const hash = require('../../../hash/hash');
-
-const NUMBER_OF_WORKERS = 5;
-let zone = napa.zone.create('zone', { workers: NUMBER_OF_WORKERS });
 
 exports.signup = (req, res, next) => {
   const {
@@ -36,27 +31,6 @@ exports.signup = (req, res, next) => {
 };
 
 exports.all = async (req, res, next) => {
-  const TEST_ITERATIONS = 1000;
-  const text = 'habr';
-
-  let startTime = Date.now();
-  for (let i = 0; i < TEST_ITERATIONS; i++) {
-    hash.MD5(text);
-    hash.SHA256(text);
-  }
-  console.log('\n-----------\nsync test iterations: ' + TEST_ITERATIONS + ', timestamp: ' + (Date.now() - startTime));
-
-  const promises = [];
-  startTime = Date.now();
-  for (let i = 0; i < TEST_ITERATIONS; i++) {
-    promises.push(zone.execute((md5, sha256, text) => {
-      md5(text);
-      sha256(text);
-    }, [hash.MD5, hash.SHA256, text]));
-  }
-  await Promise.all(promises);
-  console.log('-----------\nnapajs test iterations: ' + TEST_ITERATIONS + ', timestamp: ' + (Date.now() - startTime) + ', number of workers: ' + NUMBER_OF_WORKERS);
-  console.log('-----------\n');
   User.find().then((users) => {
     res.json({
       success: true,
